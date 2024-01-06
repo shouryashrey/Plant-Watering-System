909948409bbdb1a9214951f996ed9d75ff9d05a5
Shrey Shourya <shouryashrey@gmail.com>
2024-01-07 00:13:31 +0530

    waterPlants api exposed

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(activePin, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
+
+def waterPlants(seconds):
+ GPIO.output(activePin, GPIO.HIGH) # Turn on
+ sleep(seconds) # Sleep for 1 second
+ GPIO.output(activePin, GPIO.LOW) # Turn off
+
 while True: # Run forever
  GPIO.output(activePin, GPIO.HIGH) # Turn on
  sleep(1) # Sleep for 1 second
---
+++ webController.py
@@ -0,0 +1,31 @@
+# Using flask to make an api 
+# import necessary libraries and functions 
+from main import waterPlants
+from flask import Flask, jsonify, request 
+  
+# creating a Flask app 
+app = Flask(__name__) 
+  
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/checkStatus', methods = ['GET']) 
+def home(): 
+    if(request.method == 'GET'): 
+        data = "System is LIVE!"
+        return jsonify({'data': data}) 
+  
+  
+# A simple function to calculate the square of a number 
+# the number to be squared is sent in the URL when we use GET 
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10 
+# this returns 100 (square of 10) 
+@app.route('/waterPlants/<int:seconds>', methods = ['GET']) 
+def disp(seconds): 
+    waterPlants(seconds)
+    return jsonify({'data': 'waterring the plants for' + seconds}) 
+  
+  
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(debug = True, port=8080)
